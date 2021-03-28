bef6c52c45ed6149d0a5325446adf9da05f31a51
wfz <2693281866@qq.com>
2021-03-28 23:43:11 +0800

    提交测试

--- spring-boot-mybatis/src/test/java/com/ffz/boot/mybatis/mapper/ClazzMapperTest.java
+++ spring-boot-mybatis/src/test/java/com/ffz/boot/mybatis/mapper/ClazzMapperTest.java
@@ -1,5 +1,13 @@
 package com.ffz.boot.mybatis.mapper;
 
+import com.ffz.boot.mybatis.domain.Clazz;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.annotation.Resource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -8,6 +16,27 @@ import static org.junit.jupiter.api.Assertions.*;
  * Data 2021-03-28
  * Description
  */
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
 class ClazzMapperTest {
+    @Resource
+    private ClazzMapper clazzMapper;
 
+    @Test
+    void getClazz() {
+        Clazz clazz = clazzMapper.getClazz(1);
+        assertEquals("软件1921", clazz.getClazzName());
+        assertEquals(1, clazz.getClazzId());
+        assertEquals(1, clazz.getTeacher().getTeacherId());
+        assertEquals("徐老师", clazz.getTeacher().getTeacherName());
+        assertEquals(6, clazz.getStudents().size());
+
+
+        System.out.println("班级信息:");
+        System.out.println(clazz.getClazzId() + "," + clazz.getClazzName());
+        System.out.println("班级教师信息：");
+        System.out.println(clazz.getTeacher().getTeacherId() + "," + clazz.getTeacher().getTeacherName());
+        System.out.println("班级学生:");
+        clazz.getStudents().forEach(student -> System.out.println(student.getStudentName() + "," + student.getHometown()));
+    }
 }
--- spring-boot-mybatis/src/test/java/com/ffz/boot/mybatis/mapper/Teacher1MapperTest.java
+++ spring-boot-mybatis/src/test/java/com/ffz/boot/mybatis/mapper/Teacher1MapperTest.java
@@ -1,13 +1,34 @@
 package com.ffz.boot.mybatis.mapper;
 
+import com.ffz.boot.mybatis.domain.Student1;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.annotation.Resource;
+
 import static org.junit.jupiter.api.Assertions.*;
-
 /**
- * Classname com.ffz.boot.mybatis.mapper.Teacher1MapperTest
- * Author ffz
- * Data 2021-03-28
- * Description
- */
-class Teacher1MapperTest {
+  *Classname com.ffz.boot.mybatis.mapper.Teacher1MapperTest
+  *Author ffz
+  *Data 2021-03-28
+  *Description
+  */
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+class Student1MapperTest {
+    @Resource
+    private Student1Mapper student1Mapper;
 
+    @Test
+    void getStudentManyToOne() {
+        Student1 student = student1Mapper.getStudent(1001);
+        System.out.println(student);
+        assertEquals(1001, student.getStudentId());
+        assertEquals("金晨星", student.getStudentName());
+        assertEquals(1, student.getClazz().getClazzId());
+        assertEquals("软件1921",student.getClazz().getClazzName());
+        assertEquals(2,student.getCourses().size());
+    }
 }
