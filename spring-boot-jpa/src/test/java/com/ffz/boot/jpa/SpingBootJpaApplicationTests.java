d90c7234c1677c90bef95536029bb7adf2c3f36a
wfz <2693281866@qq.com>
2021-03-30 10:38:40 +0800

    jpa模块

---
+++ spring-boot-jpa/src/test/java/com/ffz/boot/jpa/SpingBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ffz.boot.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingBootJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
