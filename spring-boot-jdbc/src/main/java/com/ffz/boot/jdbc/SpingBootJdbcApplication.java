f34eb7794888bca53e29b9fb3d77df062ab4f99a
wfz <2693281866@qq.com>
2021-03-30 10:38:07 +0800

    jdbc模块

---
+++ spring-boot-jdbc/src/main/java/com/ffz/boot/jdbc/SpingBootJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.ffz.boot.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBootJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingBootJdbcApplication.class, args);
+    }
+
+}
